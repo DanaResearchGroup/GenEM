acbe18f733ae64f90ac4fda34bd2d8f2b3a256e0
samcogan21 <161603591+samcogan21@users.noreply.github.com>
2024-11-04 11:16:34 +0200

    Create optimize.py
    This class is our implementation of a genetic algorithm optimization using the mutations we defines earlier.

---
+++ code/optimize.py
@@ -0,0 +1,171 @@
+import numpy as np
+from rdkit import Chem
+from molecules import Molecule
+from mol_space import MolecularSpace
+from rdkit.Chem import BRICS
+
+
+class AdvancedPopulation:
+    """
+    Class AdvancedPopulation
+    Argument:
+        initial_smiles(list): initial smiles will represent the initial population for optimization
+    """
+    def __init__(self, initial_smiles):
+        self.population = [Molecule(smiles) for smiles in initial_smiles]
+        self.size = len(self.population)
+        self.best_solution = None
+        self.best_fitness = -float('inf')
+
+    def get_individuals(self, index):
+        """
+        Function to get individual from population
+        Argument:
+            index (int): index of population
+        Returns:
+            individual at certain index
+        """
+        return self.population[index]
+
+    def update_individual(self, index, new_molecule):
+        """
+        Function to update individual from population
+        Argument:
+            index (int): index of population
+            new_molecule (AdvancedMolecule): new molecule
+        Returns:
+            new_molecule (AdvancedMolecule): updated molecule
+        """
+        self.population[index] = new_molecule
+
+    def calculate_population_fitness(self, objective_function):
+        """
+        Function to calculate population fitness using a provided objective function
+        Argument:
+            objective_function (function): objective function
+        Returns:
+            None
+        """
+        for mol in self.population:
+            fitness = objective_function(mol)
+            if fitness > self.best_fitness:
+                self.best_fitness = fitness
+                self.best_solution = mol
+
+class MolecularDifferentialEvolution:
+    """
+    Class MolecularDifferentialEvolution
+    Argument:
+        objective_function (function): objective function
+        initial_smiles (list): initial smiles will represent the initial population for optimization
+        crossover_prob (float): crossover probability
+        max_iter (int): maximum number of iterations
+    """
+    def __init__(self, objective_function, initial_population, crossover_prob=0.7, max_iter=1000):
+        self.objective_function = objective_function
+        self.population_size = len(initial_population)
+        self.crossover_prob = crossover_prob
+        self.max_iter = max_iter
+        self.advanced_molecular_space = MolecularSpace(initial_population)
+        self.best_solution = None
+        self.best_fitness = -float('inf')  # Start with negative infinity for maximization goal
+        # Print initial fitness values for each molecule
+        self.print_initial_fitness()
+
+    def print_initial_fitness(self):
+        """
+        Function for printing initial fitness values of population
+        Returns:
+            prints initial fitness values of population
+        """
+        print("Initial fitness values of starting molecules:")
+        for i, molecule in enumerate(self.advanced_molecular_space.population):
+            fitness = self.objective_function(molecule)
+            print(f"Molecule {i+1}: SMILES={molecule.smiles}, Fitness={fitness}")
+
+
+    def mutate(self, idx):
+        """
+        Function to mutate molecule
+        Argument:
+            idx (int): index of molecule
+        Returns:
+            mutated molecule if mutation was successful, otherwise target (original molecule)
+        """
+        target = self.advanced_molecular_space.population[idx]
+        mutant = self.advanced_molecular_space.mutate_molecule(target)
+
+        if mutant is None:
+            return target  # Fallback to target molecule if mutation fails
+
+        return mutant
+
+    def crossover(self, target, mutant):
+        """
+        Function for molecule crossover
+        Argument:
+            target (AdvancedMolecule): original molecule
+            mutant (AdvancedMolecule): mutated molecule
+        Returns:
+            trial molecule if crossover was successful, otherwise target (original molecule)
+        """
+        try:
+            target_smiles = target.smiles
+            mutant_smiles = mutant.smiles
+
+            target_mol = Chem.MolFromSmiles(target_smiles)
+            mutant_mol = Chem.MolFromSmiles(mutant_smiles)
+
+            if target_mol is None or mutant_mol is None:
+                return target  # Fallback to target if conversion fails
+
+            crossover_mask = np.random.rand(target_mol.GetNumAtoms()) < self.crossover_prob
+            trial_mol = Chem.RWMol(target_mol)
+
+            for i in range(trial_mol.GetNumAtoms()):
+                if crossover_mask[i]:
+                    trial_mol.ReplaceAtom(i, mutant_mol.GetAtomWithIdx(i))
+
+            Chem.SanitizeMol(trial_mol)  # Try to sanitize
+            trial_smiles = Chem.MolToSmiles(trial_mol)
+            return Molecule(trial_smiles)
+        except Exception as e:
+            return target  # Return target if trial molecule is invalid
+
+    def select(self, idx, trial):
+        """
+        Function to select a molecule by fitness score of objective function
+        Argument:
+            idx (int): index of molecule
+            trial (AdvancedMolecule): trial molecule, which was created during crossover by combined mutated molecule
+            and original molecule
+        Returns:
+            trial molecule if its fitness was better than the target molecule, otherwise target (original molecule)
+        """
+        target = self.advanced_molecular_space.population[idx]
+        target_fitness = self.objective_function(target)
+        trial_fitness = self.objective_function(trial)
+
+        if trial_fitness > target_fitness:
+            self.advanced_molecular_space.population[idx] = trial
+
+        if trial_fitness > self.best_fitness:
+            self.best_fitness = trial_fitness
+            self.best_solution = trial
+
+    def run(self):
+        """
+        Function to run optimization
+        Returns:
+            best_solution (AdvancedMolecule)
+            best_fitness (float)
+            count (int): number of unsuccessful crossovers
+        """
+        for iteration in range(self.max_iter):
+            for i in range(self.population_size):
+                target = self.advanced_molecular_space.population[i]
+                mutant = self.mutate(i)
+                trial = self.crossover(target, mutant)
+                self.select(i, mutant)
+
+        return self.best_solution, self.best_fitness
