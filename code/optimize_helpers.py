7f46d68eb051744e8bcc2a305131705e13f43ea5
samcogan21 <161603591+samcogan21@users.noreply.github.com>
2024-11-12 15:34:08 +0200

    Create optimize_helpers.py which includes the mutation logic

---
+++ code/optimize_helpers.py
@@ -0,0 +1,25 @@
+import random
+from molecules import Molecule
+
+def mutate_molecule(molecule: Molecule) -> Molecule:
+    """
+    Function to mutate a molecule. It works by going through the list of mutations, randomly picking one. If the
+    mutation results in a valid molecule, it returns the mutated molecule. Otherwise, it will randomly pick another
+    mutation, and repeat the process. If the mutation doesn't produce a valid molecule after 3 times,
+    it returns the original molecule.
+    Argument:
+        molecule(Molecule): Molecule which is represented as fingerprint
+    Return:
+        mutated_molecule(Molecule): Mutated molecule
+    """
+    mutation_strategies = [
+        molecule.smart_atom_substitution,
+        molecule.isostere_replacement,
+        molecule.functional_group_addition
+    ]
+    for _ in range(3):
+        strategy = random.choice(mutation_strategies)
+        mutated_molecule = strategy(molecule)
+        if mutated_molecule and mutated_molecule.mol:
+            return mutated_molecule
+    return molecule
