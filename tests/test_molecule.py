d86b42eb02f8f3b39e209e9901c258bea776a2a8
samcogan21 <161603591+samcogan21@users.noreply.github.com>
2024-11-25 15:30:31 +0200

    Updated tests with improved logic + removed outdated tests

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -3,15 +3,12 @@
 
 import unittest
 
+from numpy.ma.testutils import assert_not_equal
+
 from constants import MOLECULES_SMILES
 from rdkit import Chem
 
-from ... import (
-    Molecule,
-    functional_group_addition,
-    isostere_replacement,
-    smart_atom_substitution,
-)
+from molecules import Molecule
 
 
 class TestMolecule(unittest.TestCase):
@@ -20,10 +17,8 @@ class TestMolecule(unittest.TestCase):
         self.ethanol_smiles = MOLECULES_SMILES.get("Ethanol")  # Ethanol
         self.tnt_smiles = MOLECULES_SMILES.get("TNT")  # TNT
         self.invalid_smiles = "COCOCOCOCOC11##111OOO"  # invalid smile
-
         self.ethanol = Molecule(self.ethanol_smiles)
         self.tnt = Molecule(self.tnt_smiles)
-        self.invalid_molecule = Molecule(self.invalid_smiles)
 
     def test_smiles_to_mol(self):
         expected_eth_smiles = Chem.CanonSmiles(
@@ -35,31 +30,30 @@ class TestMolecule(unittest.TestCase):
         self.assertEqual(generated_eth_smiles, expected_eth_smiles)
         self.assertEqual(generated_tnt_smiles, expected_tnt_smiles)
 
-    def test_fingerprint(self):
-        self.ethanol.calculate_fingerprint()
-        self.assertIsNotNone(self.ethanol.fingerprint)
-        self.assertEqual(len(self.ethanol.fingerprint), 1024)  # Default nBits=1024
-
-    def _run_mutation_test(self, mutation_func):
-        for mol in [self.ethanol, self.tnt]:
-            mutated_mol = mutation_func(mol)
-            # make sure were not getting None from mutation
-            self.assertNotEqual(mutated_mol, None)
-
-    def test_mutate_molecule(self):
-        # mutate_molecule should produce 100% valid molecules (either mutated or original)
-        self._run_mutation_test(self.molecular_space.mutate_molecule)
-
     def test_smart_atom_substitution(self):
-        self._run_mutation_test(
-            smart_atom_substitution,
-        )
+        for mol in [self.ethanol, self.tnt]:
+            mutated_mol = Molecule.smart_atom_substitution(mol.mol)
+            # make sure were not getting None from mutation
+            if mutated_mol is not None:
+                print(Chem.MolToSmiles(mutated_mol.mol))
+                self.assertNotEqual(mol.mol.ToBinary(), mutated_mol.mol.ToBinary())
+
 
     def test_isostere_replacement(self):
-        self._run_mutation_test(isostere_replacement)
+        for mol in [self.ethanol, self.tnt]:
+            mutated_mol = Molecule.isostere_replacement(mol.mol)
+            # make sure were not getting None from mutation
+            if mutated_mol is not None:
+                print(Chem.MolToSmiles(mutated_mol.mol))
+                self.assertNotEqual(mol.mol.ToBinary(), mutated_mol.mol.ToBinary())
 
     def test_functional_group_addition(self):
-        self._run_mutation_test(functional_group_addition)
+        for mol in [self.ethanol, self.tnt]:
+            mutated_mol = Molecule.functional_group_addition(mol.mol)
+            # make sure were not getting None from mutation
+            if mutated_mol is not None:
+                print(Chem.MolToSmiles(mutated_mol.mol))
+                self.assertNotEqual(mol.mol.ToBinary(), mutated_mol.mol.ToBinary())
 
 
 if __name__ == "__main__":
